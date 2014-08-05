9a50c718ec5a0744a37326f198e6915ff782eac3
wclark3 <wclark3@gmail.com>
2014-08-05 02:22:49 +0000

    new homepage

---
+++ public/javascripts/home-script.js
@@ -0,0 +1,7 @@
+$(function() {
+    
+    $('#ca-goto').height($('#ca-info').height());
+    $('#dig-goto').height($('#dig-info').height());
+    $('#dignar-goto').height($('#dignar-info').height());
+    
+});
