b8b5c375fe9fa45b5556646633fd82a2da42b2ad
wclark3 <wclark3@gmail.com>
2014-08-06 13:44:38 +0000

    fixing hot issues

--- public/javascripts/shock-script.js
+++ public/javascripts/shock-script.js
@@ -55,107 +55,43 @@ $(function() {
 		});
 	    $("#" + shock + "-perm-table-" + (i+1))
 		.css("padding-left",colWidth());
+	}	
+    }
+    
+    $('#accordion a').click(function(e) {
+
+	// see which shock table is visible (before closing)
+	var open = '';
+	for (shock in shockList) {
+	    if ($('#collapse-' + shock).hasClass('in')) {
+		open = shock }
 	}
 	
-    }
+	// hide the table of the open shock
+	$("#" + open + "-temp-table").hide();
+	$("#" + open + "-perm-table-1").hide();
+	$("#" + open + "-perm-table-2").hide();
 
-    /* populate the shockTables div with a series of handsontable 
-       objects that match up with the list of shocks */
-    /*$.get("/getShockList",
-	  { model : whichModel },
-	  function(shockList) {
+    });
 
-	      // call server to get list of equations
-	      var eqnsList;
-	      $.ajax({
-		  data: { model : whichModel },
-		  url: "/getEqnList",
-		  success: function(res) {
-		      eqnsList = res;
-		  },
-		  async: false
-	      });
-	      var eqnsDesc = Object.keys(eqnsList);
-	      var eqnsTex = [];
-	      for (var key in eqnsList) {
-		  eqnsTex.push(eqnsList[key]); }
+    $('#accordion').on('shown.bs.collapse', function(e) {
+	// e.g. e.target.id = collapse-iz
 
-	      // 1 -- add a div for each shock table to be stored
-	      var htmlDivs = "";
-	      for (shock in shockList) {
-		  htmlDivs += makeHeader(shockList[shock], eqnsTex);
-		  htmlDivs += "<div class=\"temp-shock-holder\" id=\"";
-		  htmlDivs += shock; 
-		  htmlDivs += "-temp-table\"></div>";
+	// see which shock table is visible (after opening)
+	var open = e.target.id.replace('collapse-', '');
+	/*$("#" + open + "-temp-table")
+	    .handsontable({
+		data       : [ makeZeroArray(T) ],
+		colHeaders : makeSeqArray(T)
+	    });*/
 
-		  for (var i=0; i<2; i++) {
-		      htmlDivs += "<div class=\"perm-shock-holder\" id=\"";
-		      htmlDivs += shock; 
-		      htmlDivs += "-perm-table-" + (i+1) + "\"></div>"; }
-	      }
-	      $("#shockTables").html(htmlDivs);
+	// show the table of the open shock
+	$("#" + open + "-temp-table").show();
+	$("#" + open + "-perm-table-1").show();
+	$("#" + open + "-perm-table-2").show();
 
-	      // 2 -- add the handsontable to each corresponding div
-	      // table will fill width of viewspace, or be set to 30 if it is 
-	      // too wide
-	      for (shock in shockList) {
-		  $("#" + shock + "-temp-table")
-		      .handsontable({
-			  data       : [ makeZeroArray(T) ],
-			  colHeaders : makeSeqArray(T),
-			  colWidths  : colWidth()
-		      });
-		  for (var i=0; i<2; i++) {
-		      $("#" + shock + "-perm-table-" + (i+1))
-			  .handsontable({
-			      data       : [ [0], [0], [0] ],
-			      rowHeaders : [ ["start"],["end"], ["value"] ],
-			      colWidths  : colWidth()
-			  });
-		      $("#" + shock + "-perm-table-" + (i+1))
-			  .css("padding-left",colWidth()); }
-	      }
+    });
 
-	      // make the row headers of the perm tables twice as wide as the
-	      // regular columns
-	      $(".handsontable col.rowHeader").width(colWidth()*2);	      
-	      
-	  });*/
-    
-    /* returns the larger of 30 or 1/30th of the available horizontal space 
-       this ensures there is a minimum width if the view size is small */
-    /*var colWidth = function() {
-	return Math.max(30, $(".main").width() / 30);
-    }*/
-
-    /* make a header with equation description and latex equation */
-    /*var makeHeader = function(shock, eqnsTex) {
-	var N = shock.equations.length;
-	var header = "<div class=\"shock-header\"";
-	header += "style=\"clear:left; padding-bottom:5px\">";
-	header += "<span style=\"font-weight:bold;padding-right:1em\">" + 
-	    shock.desc + "</span>";
-	
-	return header;
-    };*/
-    
-    /* create a zero array to fill the shock tables */
-    /*function makeZeroArray (n) {
-	var arr = new Array(n);
-	for (var i=0; i<n; i++) 
-	    arr[i] = 0;
-	return arr;
-    };*/
-    
-    /* create an array of sequential integers to fill the shock table 
-       column headers */
-    /*function makeSeqArray (n) {
-	var arr = new Array(n);
-	for (var i=0; i<n; i++)
-	    arr[i] = i+1;
-	return arr;
-    };*/
-    
 });
 
 /* helper method to make divs for each shock table */
